e3eb0e97d231a4069f4e433b73e1dfd1d4fc6571
andwis93 <andwis17@gmail.com>
2023-04-06 15:52:14 +0200

    gitignore change

--- src/test/java/com/vaadin/scoregoatvaadin/ScoreGoatVaadinApplicationTests.java
+++ src/test/java/com/vaadin/scoregoatvaadin/ScoreGoatVaadinApplicationTests.java
@@ -1,17 +1,10 @@
 package com.vaadin.scoregoatvaadin;
 
-import com.vaadin.scoregoatvaadin.client.ScoreGoatClient;
-import com.vaadin.scoregoatvaadin.domain.Match;
-import com.vaadin.scoregoatvaadin.domain.MatchList;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Arrays;
 
 @SpringBootTest
 class ScoreGoatVaadinApplicationTests {
-
 
 	@Test
 	void contextLoads() {
